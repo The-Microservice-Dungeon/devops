2834d1cb53c4e9c5a50badef7facb388936a3cc0
Regrets <jannik-alx@gmx.de>
2021-10-22 16:31:59 +0200

    A simple Kafka implementation in java / spring, to test connectivity. Documentation will follow asap

---
+++ kafkaClientTest/src/main/java/com/devOps/kafkaClientTest/KafkaClientTestApplication.java
@@ -0,0 +1,12 @@
+package com.devOps.kafkaClientTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaClientTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaClientTestApplication.class, args);
+	}
+}
