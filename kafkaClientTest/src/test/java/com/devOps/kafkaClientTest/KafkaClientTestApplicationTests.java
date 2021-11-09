bbb9973647228986453dfefaee4dfe5458838921
Regrets <jannik-alx@gmx.de>
2021-11-09 17:07:35 +0100

    this was messed up

--- kafkaClientTest/src/test/java/com/devOps/kafkaClientTest/KafkaClientTestApplicationTests.java
+++
@@ -1,9 +0,0 @@
-package com.devOps.kafkaClientTest;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class KafkaClientTestApplicationTests {
-    //Tests go here
-}
